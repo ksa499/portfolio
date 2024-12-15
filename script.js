6bb8a34cb22b0ef72dbd737d55f22c2a1d0e3a71
ksa499 <ksa49914@gmail.com>
2024-12-15 22:32:54 +0900

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+const myMobileMenu = document.getElementById("mobile-header");
+
+function openMobileMenu() {
+    myMobileMenu.classList.add("open");
+}
+
+function closeMobileMenu() {
+    myMobileMenu.classList.remove("open")
+}
+
+
